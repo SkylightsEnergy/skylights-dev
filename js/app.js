eb0b589429a381a83bec6d94aa6bf6fc1c1e13e6
SkylightsEnergy <skylightsenergy@gmail.com>
2021-04-28 03:59:33 -0700

    Changes

--- js/app.js
+++ js/app.js
@@ -42,6 +42,15 @@ $(".brand-slider").slick({
     ],
 });
 // end of brand slider
+// typed
+$(".element").each(function () {
+    var $this = $(this);
+    $this.typed({
+        strings: $this.attr("data-elements").split(","),
+        typeSpeed: 100, // typing speed
+        backDelay: 3000, // pause before backspacing
+    });
+});
 // testimonial slider
 $(".test-slider").slick({
     slidesToShow: 1,
